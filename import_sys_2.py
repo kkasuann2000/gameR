28c227aba893bfab355091472f73323458e7cdee
kkasuann2000 <finch.iz@mail.ru>
2024-04-13 11:26:35 +0300

    кнопки жмаются

--- import_sys_2.py
+++ import_sys_2.py
@@ -1,7 +1,7 @@
 import graphlib
 import sys
 import os
-from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QLabel, QVBoxLayout
+from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QLabel, QVBoxLayout, QScrollArea
 from PyQt6 import uic
 
 # словарь свой ств игры:
@@ -11,10 +11,10 @@ size_field = {1: 6,
               3: 10,
               4: 12}
 
-collor = {1:('light'),
-          2:('dark'),
-          3:('classic'),
-          4:('blue')}
+collor = {1:'light',
+          2:'dark',
+          3:'classic',
+          4:'blue'}
 
 
 settings = {'collor': 1, 
@@ -26,7 +26,7 @@ class pravilaWind(QWidget):
         super().__init__()
         layout = QVBoxLayout()
         self.setWindowTitle('pravila')
-        self.setWindowTitle('collor')
+        self.setWindowTitle('правила')
         self.label = QLabel(
             "Игра реверси, играется на доске 6х6; 8x8; 10х10; 12х12; клеток \nс черными и белыми фишками Игроки ходят по очереди,\nставя фишки на доску таким образом, \nчтобы окружить фишки противника. Когда фишки противника \nокружены фишками игрока с двух сторон, они переворачиваются \nна другую сторону. Цель игры - иметь больше фишек\n своего цвета на доске, чем у противника, когда все клетки\n заняты. Игра заканчивается, когда ни у одного из игроков \nнет возможности сделать ход, и побеждает \nтот, у кого больше фишек на доске.")
         layout.addWidget(self.label)
@@ -39,13 +39,28 @@ class MainWindow(QMainWindow):#глваное oкно
         uic.loadUi("C:\\Users\\finch\\OneDrive\\Рабочий стол\\pyqt progect\\gameR\\first.ui" , self)
         self.collor_vibor.hide()
 
-
-
         self.pravilaBt.clicked.connect(self.pravilno_kliknuto)
         self.collorBt.clicked.connect(self.collor_kliknuto)
 
+        global settings
+        if self.blue_Bt.clicked:
+            settings['collor'] = 4
+            print(settings)
+        elif self.light_Bt.clicked:
+            settings['collor'] = 1
+            print(settings)
+        elif self.dark_Bt.clicked:
+            settings['collor'] = 2
+            print(settings)
+        elif self.classic_Bt.clicked:
+            settings['collor'] = 3
+            print(settings)
+
     def collor_kliknuto(self):
         self.collor_vibor.setVisible(True)
+
+
+
         
        
     
