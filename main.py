866ae5b054edfed3e7f7e38c716ecc83d2bcb5eb
kkasuann2000 <finch.iz@mail.ru>
2024-05-05 11:36:35 +0300

    mashtab collor
    онбовление кнопок маштаб и калор, изменение папки

--- main.py
+++ main.py
@@ -32,34 +32,73 @@ class pravilaWind(QWidget):
             "Игра реверси, играется на доске 6х6; 8x8; 10х10; 12х12; клеток \nс черными и белыми фишками Игроки ходят по очереди,\nставя фишки на доску таким образом, \nчтобы окружить фишки противника. Когда фишки противника \nокружены фишками игрока с двух сторон, они переворачиваются \nна другую сторону. Цель игры - иметь больше фишек\n своего цвета на доске, чем у противника, когда все клетки\n заняты. Игра заканчивается, когда ни у одного из игроков \nнет возможности сделать ход, и побеждает \nтот, у кого больше фишек на доске.")
         layout.addWidget(self.label)
         self.setLayout(layout)
+        
 
 
 class MainWindow(QMainWindow):#глваное oкно
     def __init__(self):
         super().__init__()
-        uic.loadUi("C:\\Users\\finch\\OneDrive\\Рабочий стол\\pyqt progect\\gameR\\first.ui" , self)
+        uic.loadUi("QTdisignerrrrr\\first.ui" , self)
         self.collor_vibor.hide()
+        self.mashtab_vibor.hide()
 
         self.pravilaBt.clicked.connect(self.pravilno_kliknuto)
         self.collorBt.clicked.connect(self.collor_kliknuto)
-        self.pole66light.clicked.connect(self.pole66_light)
+        self.mashtab_Bt.clicket.connect(self.mashtab_kliknuto)
+       
+        self.classic_Bt.clicked.connect(self.classic_Bt_clicnuto)
+        self.light_Bt.clicked.connect(self.light_Bt_clicnuto)
+        self.blue_Bt.clicked.connect(self.blue_Bt_clicnuto)
+        self.dark_Bt.clicked.connect(self.dark_Bt_clicnuto)
 
+        self.x6.clicked.connect(self.classic_Bt_clicnuto)
+
+
+
+
+    def classic_Bt_clicnuto(self):
         global settings
-        if self.blue_Bt.clicked:
-            settings['collor'] = 4
-            print(settings)
-        elif self.light_Bt.clicked:
-            settings['collor'] = 1
-            print(settings)
-        elif self.dark_Bt.clicked:
-            settings['collor'] = 2
-            print(settings)
-        elif self.classic_Bt.clicked:
-            settings['collor'] = 3
+        settings['collor'] = 1
+        self.collor_vibor.setVisible(False)
+    
+    def light_Bt_clicnuto(self):
+        global settings
+        settings['collor'] = 3
+        self.collor_vibor.setVisible(False)
+
+    def blue_Bt_clicnuto(self):
+        global settings
+        settings['collor'] = 4
+        self.collor_vibor.setVisible(False)
+
+    def dark_Bt_clicnuto(self):
+        global settings
+        settings['collor'] = 2
+        self.collor_vibor.setVisible(False)
+        
+
+     
+
+        # global settings
+        # if self.blue_Bt.clicked:
+        #     settings['collor'] = 4
+        #     print(settings)
+        # elif self.light_Bt.clicked:
+        #     settings['collor'] = 1
+        #     print(settings)
+        # elif self.dark_Bt.clicked:
+        #     settings['collor'] = 2
+        #     print(settings)
+        # elif self.classic_Bt.clicked:
+        #     settings['collor'] = 3
         
                  
     def collor_kliknuto(self):
         self.collor_vibor.setVisible(True)
+        self.classic_Bt.setEnabled(True)
+        self.light_Bt.setEnabled(True)
+        self.dark_Bt.setEnabled(True)
+        self.blue_Bt.setEnabled(True)
 
        
     
@@ -69,9 +108,6 @@ class MainWindow(QMainWindow):#глваное oкно
         self.window.show()
 
 
-    def pole66_light(self):
-        super().__init__()
-        uic.loadUi("C:\\Users\\finch\\OneDrive\\Рабочий стол\\pyqt progect\\gameR\\pole66_light" , self)
 
 
 app = QApplication(sys.argv)
